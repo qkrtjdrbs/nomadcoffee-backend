0c5eb4f1040042dde38925c7735218c4e0660a97
qkrtjdrbs <notlce@naver.com>
2021-06-03 14:42:22 +0900

    no playgorund

--- src/server.js
+++ src/server.js
@@ -11,8 +11,6 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   uploads: false,
-  playground: true,
-  introspection: true,
   context: async ({ req }) => {
     return { loggedInUser: await getUser(req.headers.token) };
   },

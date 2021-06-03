e1220694b7250148d15d71e8567202b7cbb231af
qkrtjdrbs <notlce@naver.com>
2021-06-03 14:19:32 +0900

    introspection

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const apollo = new ApolloServer({
   typeDefs,
   uploads: false,
   playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return { loggedInUser: await getUser(req.headers.token) };
   },
